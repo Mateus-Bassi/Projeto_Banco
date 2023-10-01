711cdc714fe3c7ea34ec5ef78b59056adf7129ab
ViniciusSito <itoviniciusshigueyuki@gmail.com>
2023-10-01 12:31:50 -0300

    Configurando Models

---
+++ Projeto Cs/BancoAPI/Models/Investimento.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+
+namespace BancoAPI.Models
+{
+    //Declaração do enum dos investimentos especificos
+    public enum TipoInvestimento
+    {
+        CDB,
+        Tesouro_Direto,
+        Fundo_Imobiliario
+    }
+    public class Investimento
+    {
+        //Chave primária
+        public int InvestimentoID {get; set;}
+
+        public TipoInvestimento Tipo {get; set;}
+        public decimal ValorInicial {get; set;}
+        public DateTime DataInvestimento {get; set;}
+        public decimal Rentabilidade_Mensal {get; set;}
+        public DateTime DataRegaste {get; set;}
+
+        //Chave estrangeira para associar uma conta ao investimento 
+        public int ContaID {get; set;}
+
+        //Construtor da Classe
+        public Investimento(int investimentoID, TipoInvestimento tipo, decimal valorinicial, DateTime datainvestimento, decimal rentabilidademensal, DateTime dataregaste, int contaID)
+        //Declaração do construtor com os parametros
+        {
+            InvestimentoID = investimentoID;
+            Tipo = tipo;
+            ValorInicial = valorinicial;
+            DataInvestimento = datainvestimento;
+            Rentabilidade_Mensal = rentabilidademensal;
+            DataRegaste = dataregaste;
+            ContaID = contaID;
+        }
+    }
+}
