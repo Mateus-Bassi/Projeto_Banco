a57c35ecdcd5c73adf36fb248bf628ae65eb1876
JuanVP4 <144619288+JuanVP4@users.noreply.github.com>
2023-10-05 20:15:35 -0300

    Update ControllerEndereco.cs

--- Projeto Cs/BancoAPI/Controllers/ControllerEndereco.cs
+++ Projeto Cs/BancoAPI/Controllers/ControllerEndereco.cs
@@ -1,7 +1,10 @@
+using BancoAPI.Data;
 using BancoAPI.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace BancoAPI.Controllers
 {
@@ -9,50 +12,92 @@ namespace BancoAPI.Controllers
     [ApiController]
     public class ControllerEndereco : ControllerBase
     {
-        
-        [HttpGet]
-        public ActionResult<IEnumerable<Endereco>> Get()
-        {
-            
-            List<Endereco> enderecos = new List<Endereco>
-            {
-                //
-            };
+        private readonly BancoDbContext _context;
 
+        public ControllerEndereco(BancoDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Endereco>>> Get()
+        {
+            if (_context is null || _context.Endereco is null)
+            {
+                return BadRequest("Contexto ou Endereco não encontrado");
+            }
+
+            var enderecos = await _context.Endereco.ToListAsync();
             return Ok(enderecos);
         }
 
-        
         [HttpGet("{id}")]
-        public ActionResult<Endereco> Get(int id)
+        public async Task<ActionResult<Endereco>> Get(int id)
         {
-            //
-            return Ok();
-            
+            if (_context is null || _context.Endereco is null)
+            {
+                return BadRequest("Contexto ou Endereco não encontrado");
+            }
+
+            var endereco = await _context.Endereco.FindAsync(id);
+            if (endereco is null)
+            {
+                return NotFound();
+            }
+
+            return endereco;
         }
 
-        
         [HttpPost]
-        public IActionResult Post(Endereco endereco)
+        public async Task<IActionResult> Criar(int EnderecoID, Endereco endereco)
         {
-            //
-            return CreatedAtAction("Get", new { id = endereco.EnderecoID }, endereco);
+            if (_context is null || _context.Endereco is null)
+            {
+                return BadRequest("Contexto ou Endereco não encontrado");
+            }
+
+             _context.Endereco.Add(endereco);
+        await _context.SaveChangesAsync();
+
+        return Created("", endereco);
         }
 
-        
         [HttpPut("{id}")]
-        public IActionResult Put(int id, Endereco endereco)
+        public async Task<IActionResult> PutAsync(int id, Endereco endereco)
         {
-            //
-            return NoContent();
-        }
+            // Atribuindo para o Endereco especifico os atributos do objeto endereco
+            if(_context is null) return NotFound();
+            if(_context.Endereco is null) return NotFound();
+        
+            var enderecoExistente = await _context.Endereco.FindAsync(id);
+            if(enderecoExistente is null) return NotFound();
 
         
-        [HttpDelete("{id}")]
-        public IActionResult Delete(int id)
-        {
-            //
-            return NoContent();
+            enderecoExistente.Rua = endereco.Rua;
+            enderecoExistente.Numero = endereco.Numero;
+            enderecoExistente.Bairro = endereco.Bairro;
+            enderecoExistente.Cidade = endereco.Cidade;
+            enderecoExistente.Estado = endereco.Estado;
+            enderecoExistente.CEP = endereco.CEP;
+
+            _context.Endereco.Update(enderecoExistente);
+            await _context.SaveChangesAsync();
+
+            return Ok();
         }
+
+    [HttpDelete]
+    [Route("deletar/{EnderecoID}")]
+    public async Task<ActionResult> Deletar(int enderecoID)
+    {   // deleta o endereco especificado pelo ID
+        var v_endereco = await _context.Endereco.FindAsync(enderecoID);
+        if (v_endereco is null) return NotFound();
+
+        _context.Endereco.Remove(v_endereco);
+
+        await _context.SaveChangesAsync();
+        return Ok();
+    
     }
+}
 }
